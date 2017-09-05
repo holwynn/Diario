e9f3824e3f1f457995ca12b4c287a0609475b92e
holwynn <arielhr@openmailbox.org>
2017-09-05 16:46:11 -0300

    set default sql string length to 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // http://i.imgur.com/bXahP6S.jpg
+        Schema::defaultStringLength(191);
     }
 
     /**
