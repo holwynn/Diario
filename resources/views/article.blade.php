5a15867d349663852c0b88dd02765fadef325c57
holwynn <arielhr@openmailbox.org>
2017-09-17 09:43:14 -0300

    frontend style modifications

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -15,21 +15,19 @@
 @section('content')
 <div class="row article-page">
   <div class="col-sm-8 col-xs-12">
-    <div class="article-author-date">
-      <p>{{ $article->created_at->formatLocalized(__('newspaper.date')) }}</p>
-      <p>{{ ucfirst($article->category->name) }}</p>
-    </div>
-
     @can('edit', $article)
     <a href="{{ route('dashboard.articles.edit', ['id' => $article->id]) }}">Edit this article</a>
     @endcan
+
+    <p>{{ $article->created_at->formatLocalized(__('newspaper.date')) }}</p>
     
     <h1 class="article-title">{{ $article->title }}</h1>
     <h3 class="article-slug">{{ $article->slug }}</h3>
 
     <img src="{{ asset('/storage/' . $article->image) }}" class="img-responsive article-image image-shadow" alt="">
-    
-    <hr>
+  
+    <hr class="soft-hr">
+
     <div class="row article-content">
       <div class="col-md-12">
         {!! $article->content !!}
@@ -66,13 +64,12 @@
   <h3 class="text-center">{{ __('newspaper.related') }}</h3>
   <hr>
   @foreach ($relatedArticles as $article)
-  <div class="col-sm-4 col-xs-6">
-    <a href="{{ route('article', ['id' => $article->id, 'title' => $article->seoUrl()])}}" class="title">
-      <img src="{{ asset('storage/thumb_'.$article->image) }}" class="img-responsive image-shadow" alt="">
-      <h4 class="title">{{ $article->title }}</h4>
-    </a>
-    <h4><small>{{ $article->slug }}</small></h4>
-  </div>
+  <a href="{{ route('article', ['id' => $article->id, 'title' => $article->seoUrl()])}}" class="title">
+    <div class="col-sm-4 col-xs-6 article-related-box">
+      <h4 class="title article-related-box-title">{{ $article->title }}</h4>
+      <img src="{{ asset('/storage/' . $article->image) }}" class="img-responsive article-image image-shadow" alt="">
+    </div>
+  </a>
   @endforeach
   @endif
 </div>
