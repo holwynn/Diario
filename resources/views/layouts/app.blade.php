37b2a8ce654f175a5524122c921ca95b29d92d05
holwynn <arielhr@openmailbox.org>
2017-09-18 11:04:27 -0300

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <link rel="stylesheet" href="/css/styles.css">
 
-  <link rel="icon" type="image/x-icon" href="" />
+  <link rel="icon" href="/favicon.png">
 </head>
 <body>
   <div class="container">
