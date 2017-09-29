d950e501bc332b2a596236a7e23c49faef1b7467
holwynn <arielhr@openmailbox.org>
2017-09-29 18:46:54 -0300

    wip login page

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('title', config('newspaper.name'))</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Raleway:300,400" rel="stylesheet">
+
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="/assets/dashboard/css/simple-line-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="/css/styles.css">
+
+  <link rel="icon" href="/favicon.png">
+</head>
+<body>
+  <div class="container">
+    <div class="col-md-12 text-center">
+      <h1>{{ config('app.name') }} Login</h1>
+    </div>
+  </div>
+
+  <div class="container" style="margin-top: 25px;">
+    @yield('content')
+  </div>
+  @yield('javascripts')
+</body>
+</html>
